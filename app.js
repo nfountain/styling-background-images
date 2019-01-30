a198e21da99036a266ef5511919188572fbb56e6
Nicena Fontaine <nicenas.email@gmail.com>
2019-01-30 17:29:09 -0500

    Declare initial variables and create function for fullscreen background image button.

--- app.js
+++ app.js
@@ -1,3 +1,48 @@
 // Global variables
-var body = document.getElementById('body');
+
 var bttnFullScreen = document.getElementById('full-screen');
+var bttnRepeatImg = document.getElementById('repeat');
+var isVisible = false;
+
+var setFullScrn = function () {
+    var loadImg = function () {
+        bttnFullScreen.innerText = "Loading Image";
+        isVisible = true;
+        document.body.style.backgroundImage = "url('https://images.unsplash.com/photo-1548620848-d375c7919ea2?ixlib=rb-1.2.1&auto=format&fit=crop&w=7s50&q=80')";
+        console.log(isVisible);
+    }
+    loadImg();
+    setTimeout(function () {
+        bttnFullScreen.innerText = "Full Screen Image";
+    }, 500);
+};
+
+// Event Listeners
+
+bttnFullScreen.addEventListener('click', setFullScrn);
+
+
+/*
+TODO: change function of button to hide the image, as well. (And change the setTimeout bttnFullScreen.innerText = "Full Screen Image" to say "Hide Image".)
+TODO: Turn button function into a factory object and create a new object for each button, passing the parameters of the image/styling
+// Return to default attempt
+var clickToHide = function () {
+    bttnFullScreen.innerText = "Full Screen Image";
+    document.body.style.backgroundImage = "none";
+    isVisible = false;
+}
+// FACTORY FUNCTION ATTEMPT
+var changeBackgroundImg = {};
+changeBackgroundImg.body = body;
+changeBackgroundImg.style = body.style('background-color: purple');
+changeBackgroundImg.showBackgroundImg = function () {
+    body.style.backgroundImage('none');
+    isVisible = false;
+    this.button.innerText = "";
+}
+changeBackgroundImg.hideBackgroundImg = function () {
+    body.style.backgroundImage('none');
+    isVisible = false;
+    this.button.innerText = "";
+}
+*/
