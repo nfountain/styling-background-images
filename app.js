406f906fb34e4872c011c95bb073e49686d6dcd9
Nicena Fontaine <nicenas.email@gmail.com>
2019-01-30 19:49:56 -0500

    Add removal of header and section background images to button functions.

--- app.js
+++ app.js
@@ -4,12 +4,16 @@ var bttnFullScreen = document.getElementById('full-screen');
 var bttnRepeatImg = document.getElementById('repeat');
 var bodyStyle = document.body.style;
 var container = document.getElementById('container');
+var header = document.getElementById('header');
+var refSection = document.getElementById('reference');
 var isVisible = false;
 
 var setFullScrn = function () {
     var loadImg = function () {
         bttnFullScreen.innerText = "Loading Image";
         isVisible = true;
+        header.style.backgroundImage = "none";
+        refSection.style.backgroundImage = "none";
         bodyStyle.background = "url('https://images.unsplash.com/photo-1548620848-d375c7919ea2?ixlib=rb-1.2.1&auto=format&fit=crop&w=7s50&q=80') no-repeat center center fixed";
         bodyStyle.backgroundSize = "cover";
         console.log(isVisible);
@@ -24,6 +28,8 @@ var setRepeatImg = function () {
     var loadImg = function () {
         bttnRepeatImg.innerText = "Loading Image";
         isVisible = true;
+        header.style.backgroundImage = "none";
+        refSection.style.backgroundImage = "none";
         bodyStyle.backgroundImage = "url('https://cdn.pixabay.com/photo/2015/04/23/21/59/tree-736877_960_720.jpg')";
         bodyStyle.backgroundSize = "300px auto";
         bodyStyle.backgroundRepeat = "repeat";
